ad0e282253a1b0543f4c598b571bb9ac00bb5d4b
emersongouveia <47765183+emersongouveia@users.noreply.github.com>
2022-02-03 01:26:37 -0300

    Layout e o estilo da aplicação

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -1,8 +1,9 @@
 from django.test import LiveServerTestCase
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
-import time
 from selenium.common.exceptions import WebDriverException
+import time
+
 MAX_WAIT = 5
 
 class NewVisitorTest(LiveServerTestCase):
@@ -24,7 +25,74 @@ class NewVisitorTest(LiveServerTestCase):
             except (AssertionError, WebDriverException) as e:  
                 if time.time() - start_time > MAX_WAIT:  
                     raise e  
+                time.sleep(0.5)
 
+    def test_layout_and_styling(self):
+        # Edith entra na home page
+        self.browser.get(self.live_server_url)
+        self.browser.set_window_size(1024, 768)
+
+        # Ela nota que o input box está centralizado
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        self.assertAlmostEqual(
+            inputbox.location['x'] + inputbox.size['width'] / 2,
+            512,
+            delta=10
+        )
+
+        # Ela inicia uma nova lista e nota que o input
+        # também está centralizado
+        inputbox.send_keys('testing')
+        inputbox.send_keys(Keys.ENTER)
+        self.wait_for_row_in_list_table('1: testing')
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        self.assertAlmostEqual(
+            inputbox.location['x'] + inputbox.size['width'] / 2,
+            512,
+            delta=10
+        )
+
+    def test_can_start_a_list_for_one_user(self): 
+    
+        # Maria decidiu utilizar o novo app TODO. Ela entra em sua página principal:
+        self.browser.get(self.live_server_url)
+
+        # Ela nota que o título da página menciona TODO
+        self.assertIn('To-Do', self.browser.title)
+        header_text = self.browser.find_element_by_tag_name('h1').text  
+        self.assertIn('To-Do', header_text)
+
+        # Ela é convidada a entrar com um item TODO imediatamente
+        inputbox = self.browser.find_element_by_id('id_new_item')  
+        self.assertEqual(inputbox.get_attribute('placeholder'), 'Enter a to-do item')
+
+        # Ela digita "Estudar testes funcionais" em uma caixa de texto
+        inputbox.send_keys('Estudar testes funcionais')
+
+        # Quando ela aperta enter, a página atualiza, e mostra a lista
+        # "1: Estudar testes funcionais" como um item da lista TODO
+        inputbox.send_keys(Keys.ENTER)
+        self.wait_for_row_in_list_table('1: Estudar testes funcionais')
+        
+        # Ainda existe uma caixa de texto convidando para adicionar outro item
+        # Ela digita: "Estudar testes de unidade"
+        inputbox = self.browser.find_element_by_id('id_new_item')  
+        inputbox.send_keys('Estudar testes de unidade')
+        inputbox.send_keys(Keys.ENTER)
+
+        # A página atualiza novamente, e agora mostra ambos os itens na sua lista
+        table = self.browser.find_element_by_id('id_list_table')
+        rows = table.find_elements_by_tag_name('tr')
+        self.wait_for_row_in_list_table('1: Estudar testes funcionais')
+        self.wait_for_row_in_list_table('2: Estudar testes de unidade')
+
+        # Maria se pergunta se o site vai lembrar da sua lista. Então, ela verifica que
+        # o site gerou uma URL única para ela -- existe uma explicação sobre essa feature
+
+        # Ela visita a URL: a sua lista TODO ainda está armazenada
+
+        # Satisfeita, ela vai dormir
+    
     def test_multiple_users_can_start_lists_at_different_urls(self):
         # Maria começa uma nova lista
         self.browser.get(self.live_server_url)
@@ -63,45 +131,4 @@ class NewVisitorTest(LiveServerTestCase):
         self.assertNotIn('Estudar testes funcionais', page_text)
         self.assertIn('Comprar leite', page_text)
 
-        # Satisfeitos, ambos vão dormir                
-
-    def test_can_start_a_list_for_one_user(self): 
-    
-        # Maria decidiu utilizar o novo app TODO. Ela entra em sua página principal:
-        self.browser.get(self.live_server_url)
-
-        # Ela nota que o título da página menciona TODO
-        self.assertIn('To-Do', self.browser.title)
-        header_text = self.browser.find_element_by_tag_name('h1').text  
-        self.assertIn('To-Do', header_text)
-
-        # Ela é convidada a entrar com um item TODO imediatamente
-        inputbox = self.browser.find_element_by_id('id_new_item')  
-        self.assertEqual(inputbox.get_attribute('placeholder'), 'Enter a to-do item')
-
-        # Ela digita "Estudar testes funcionais" em uma caixa de texto
-        inputbox.send_keys('Estudar testes funcionais')
-
-                # Quando ela aperta enter, a página atualiza, e mostra a lista
-        # "1: Estudar testes funcionais" como um item da lista TODO
-        inputbox.send_keys(Keys.ENTER)
-        
-        self.wait_for_row_in_list_table('1: Estudar testes funcionais')
-        
-        # Ainda existe uma caixa de texto convidando para adicionar outro item
-        # Ela digita: "Estudar testes de unidade"
-        inputbox = self.browser.find_element_by_id('id_new_item')  
-        inputbox.send_keys('Estudar testes de unidade')
-        inputbox.send_keys(Keys.ENTER)
-        
-
-        # A página atualiza novamente, e agora mostra ambos os itens na sua lista
-        self.wait_for_row_in_list_table('1: Estudar testes funcionais')
-        self.wait_for_row_in_list_table('2: Estudar testes de unidade')
-
-        # Maria se pergunta se o site vai lembrar da sua lista. Então, ela verifica que
-        # o site gerou uma URL única para ela -- existe uma explicação sobre essa feature
-
-        # Ela visita a URL: a sua lista TODO ainda está armazenada
-
-        # Satisfeita, ela vai dormir
+        # Satisfeitos, ambos vão dormir
