3029a3ff4009776b1e45d939064da8902218409c
emersongouveia <47765183+emersongouveia@users.noreply.github.com>
2022-02-03 00:53:59 -0300

    Implementando o Novo Projeto (Listas e Items) - PARTE 1

--- lists/tests.py
+++ lists/tests.py
@@ -1,57 +1,53 @@
-from django.test import LiveServerTestCase
-from selenium import webdriver
-from selenium.webdriver.common.keys import Keys
-import time
+from django.test import TestCase
+from lists.models import Item
 
-class NewVisitorTest(LiveServerTestCase):
+class HomePageTest(TestCase):
 
-    def setUp(self):
-        self.browser = webdriver.Firefox()
+    def test_uses_home_template(self):
+        response = self.client.get('/')
+        self.assertTemplateUsed(response, 'home.html')
 
-    def tearDown(self):
-        self.browser.quit()
-    def check_for_row_in_list_table(self, row_text):
-        table = self.browser.find_element_by_id('id_list_table')
-        rows = table.find_elements_by_tag_name('tr')
-        self.assertIn(row_text, [row.text for row in rows])
+    def test_can_save_a_POST_request(self):
+        self.client.post('/', data={'item_text': 'A new list item'})
 
-    def test_can_start_a_list_and_retrieve_it_later(self): 
-    
-        # Maria decidiu utilizar o novo app TODO. Ela entra em sua página principal:
-        self.browser.get(self.live_server_url)
+        self.assertEqual(Item.objects.count(), 1)
+        new_item = Item.objects.first()
+        self.assertEqual(new_item.text, 'A new list item')
 
-        # Ela nota que o título da página menciona TODO
-        self.assertIn('To-Do', self.browser.title)
-        header_text = self.browser.find_element_by_tag_name('h1').text  
-        self.assertIn('To-Do', header_text)
+    def test_redirects_after_POST(self):
+        response = self.client.post('/', data={'item_text': 'A new list item'})
 
-        # Ela é convidada a entrar com um item TODO imediatamente
-        inputbox = self.browser.find_element_by_id('id_new_item')  
-        self.assertEqual(inputbox.get_attribute('placeholder'), 'Enter a to-do item')
-
-        # Ela digita "Estudar testes funcionais" em uma caixa de texto
-        inputbox.send_keys('Estudar testes funcionais')
-
-                # Quando ela aperta enter, a página atualiza, e mostra a lista
-        # "1: Estudar testes funcionais" como um item da lista TODO
-        inputbox.send_keys(Keys.ENTER)
-        time.sleep(1)
-        self.check_for_row_in_list_table('1: Estudar testes funcionais')
+        self.assertEqual(response.status_code, 302)
+        self.assertEqual(response['location'], '/lists/the-only-list-in-the-world/')
         
-        # Ainda existe uma caixa de texto convidando para adicionar outro item
-        # Ela digita: "Estudar testes de unidade"
-        inputbox = self.browser.find_element_by_id('id_new_item')  
-        inputbox.send_keys('Estudar testes de unidade')
-        inputbox.send_keys(Keys.ENTER)
-        time.sleep(1)
+    def test_only_saves_items_when_necessary(self):
+        self.client.get('/')
+        self.assertEqual(Item.objects.count(), 0)
 
-        # A página atualiza novamente, e agora mostra ambos os itens na sua lista
-        self.check_for_row_in_list_table('1: Estudar testes funcionais')
-        self.check_for_row_in_list_table('2: Estudar testes de unidade')
+    def test_displays_all_list_items(self):
+        Item.objects.create(text='itemey 1')
+        Item.objects.create(text='itemey 2')
 
-        # Maria se pergunta se o site vai lembrar da sua lista. Então, ela verifica que
-        # o site gerou uma URL única para ela -- existe uma explicação sobre essa feature
+        response = self.client.get('/')
 
-        # Ela visita a URL: a sua lista TODO ainda está armazenada
+        self.assertIn('itemey 1', response.content.decode())
+        self.assertIn('itemey 2', response.content.decode())
 
-        # Satisfeita, ela vai dormir
+class ItemModelTest(TestCase):
+
+    def test_saving_and_retrieving_items(self):
+        first_item = Item()
+        first_item.text = 'O primeiro item'
+        first_item.save()
+
+        second_item = Item()
+        second_item.text = 'O segundo item'
+        second_item.save()
+
+        saved_items = Item.objects.all()
+        self.assertEqual(saved_items.count(), 2)
+
+        first_saved_item = saved_items[0]
+        second_saved_item = saved_items[1]
+        self.assertEqual(first_saved_item.text, 'O primeiro item')
+        self.assertEqual(second_saved_item.text, 'O segundo item')
